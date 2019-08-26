7961ed545cb768e9d87b43dea593b1e6d96fad2d
ishidafuu <ishidafuu@gmail.com>
2019-08-26 18:53:10 +0900

    [update] アニメーションから呼び出されるイベント用スクリプトおよびパラメーター修正

---
+++ Assets/AnimationEditor/PartsParmater.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.Collections;
+using UnityEditor;
+using UnityEngine;
+
+namespace YYHS
+{
+    [ExecuteInEditMode]
+    public class PartsParmater : MonoBehaviour
+    {
+        public bool m_isFlash = false;
+        public bool m_isBrink = false;
+        public Color m_color = Color.white;
+    }
+
+    [CustomEditor(typeof(PartsParmater))]
+    public class PartsParmaterEditor : Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            // if (GUILayout.Button("Init Object"))
+            //     (target as SpriteSetter).InitObject();
+
+            // if (GUILayout.Button("Inactive Character Sprite"))
+            //     (target as SpriteSetter).InactiveCharacterSprite();
+            // if (GUILayout.Button("Inactive BG Sprite"))
+            //     (target as SpriteSetter).InactiveBGSprite();
+            // if (GUILayout.Button("Inactive Effect Sprite"))
+            //     (target as SpriteSetter).InactiveEffectSprite();
+
+        }
+
+    }
+}
---
+++ Assets/GameAssets/Scripts/Animation/AnimationEvent.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace YYHS
+{
+
+    public class AnimationEvent : MonoBehaviour
+    {
+        public void EventPlaySE(int seNo)
+        {
+            // 効果音再生
+        }
+
+        public void EventEffect(string effectName)
+        {
+            // 汎用エフェクト再生（無限）
+        }
+
+        public void EventFlash()
+        {
+            // 点滅１回
+        }
+
+        public void EventBlink(int frame)
+        {
+            // 連続点滅
+        }
+
+        public void EventColor(int colorNo, float frame)
+        {
+            // 色変え
+        }
+
+        public void EventPlE(Object asdf)
+        {
+            // 効果音再生
+        }
+    }
+}
