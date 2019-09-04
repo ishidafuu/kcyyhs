3cde0add805d65e4c82be0e84d1a72ebfc3a588a
ishidafuu <ishidafuu@gmail.com>
2019-09-05 01:06:57 +0900

    [update] バトルアニメーションシステムのフレーム情報対応

--- Assets/GameAssets/Scripts/ECS/Systems/Render/BattleDrawSystem.cs
+++ Assets/GameAssets/Scripts/ECS/Systems/Render/BattleDrawSystem.cs
@@ -1,11 +1,12 @@
-﻿using Unity.Burst;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Jobs;
-// using Unity.Transforms;
+﻿using UnityEngine.Experimental.PlayerLoop;
 using UnityEngine;
 using Unity.Mathematics;
-using UnityEngine.Experimental.PlayerLoop;
+using Unity.Jobs;
+using Unity.Entities;
+using Unity.Collections;
+using Unity.Burst;
+using System.Collections.Generic;
+// using Unity.Transforms;
 
 namespace YYHS
 {
@@ -28,65 +29,79 @@ namespace YYHS
 
             int charaNo = seq.animation.charaNo;
             EnumAnimationName animName = seq.animation.animName;
-            Debug.Log(animName);
+            // Debug.Log(animName);
             YHAnimation anim = Shared.yhCharaAnimList.GetAnim(charaNo, animName);
             int count = seq.animation.count;
 
             YHFrameData emptyFrameData = new YHFrameData();
             foreach (YHAnimationParts item in anim.parts)
             {
-                YHFrameData targetFrame = emptyFrameData;
-                foreach (YHFrameData frame in item.frames)
-                {
-                    if (frame.frame > count)
-                        break;
 
-                    targetFrame = frame;
-                }
-
-                if (!targetFrame.isActive)
-                    continue;
-
-
-                Debug.Log(item.name);
                 // TODO:背景なども適切なメッシュから描画する
                 if (!Shared.charaMeshMat.meshDict.ContainsKey(item.name))
                     continue;
 
-                Mesh mesh = Shared.charaMeshMat.meshDict[item.name];
-                Material mat = Shared.charaMeshMat.materialDict[item.name];
+                YHFrameData isActive = emptyFrameData;
+                GetNowFrameData(count, item.isActive, ref isActive);
+
+                if (!isActive.value)
+                    continue;
+
+                // Debug.Log(item.name);
+
                 float posX = (item.positionX.length == 0)
                     ? 0
                     : item.positionX.Evaluate(count);
                 float posY = (item.positionY.length == 0)
                     ? 0
                     : item.positionY.Evaluate(count) + Settings.Instance.DrawPos.BgScrollY;
+
                 float scaleX = (item.scaleX.length == 0)
                     ? 1
                     : item.scaleX.Evaluate(count);
                 float scaleY = (item.scaleY.length == 0)
                     ? 1
                     : item.scaleY.Evaluate(count);
-                float rotate = (item.scaleX.length == 0)
+
+                float rotate = (item.rotation.length == 0)
                     ? 0
                     : item.rotation.Evaluate(count);
 
                 float layer = (int)EnumDrawLayer.Chara + item.orderInLayer;
 
-                int flipX = (targetFrame.isFlipX)
+                YHFrameData isFlipX = emptyFrameData;
+                GetNowFrameData(count, item.isFlipX, ref isFlipX);
+                int flipX = (isFlipX.value)
                     ? 180
                     : 0;
 
-                int flipY = (targetFrame.isFlipY)
+                YHFrameData isFlipY = emptyFrameData;
+                GetNowFrameData(count, item.isFlipY, ref isFlipY);
+                int flipY = (isFlipY.value)
                     ? +90
                     : -90;
 
+                YHFrameData isBrink = emptyFrameData;
+                GetNowFrameData(count, item.isBrink, ref isBrink);
+
+                Mesh mesh = Shared.charaMeshMat.meshDict[item.name];
+                Material mat = Shared.charaMeshMat.materialDict[item.name];
                 Matrix4x4 matrixes = Matrix4x4.TRS(
                     new Vector3(posX, posY, layer),
                     Quaternion.Euler(new Vector3(flipY, flipX, rotate)),
-                    new Vector3(scaleX, scaleY, 1));
+                    new Vector3(scaleX, 1, scaleY));
 
                 Graphics.DrawMesh(mesh, matrixes, mat, 0);
+            }
+        }
+
+        private static void GetNowFrameData(int count, List<YHFrameData> srcList, ref YHFrameData nowData)
+        {
+            foreach (var data in srcList)
+            {
+                if (data.frame > count)
+                    break;
+                nowData = data;
             }
         }
     }
