528bdd035216f05d1c0bcaabbb7d7c8d61320d1e
ishidafuu <ishidafuu@gmail.com>
2019-09-05 22:29:35 +0900

    [update] シーンセレクター追加

---
+++ Assets/GameAssets/Scripts/Editor/SceneSelector.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+
+public class SceneSelector : Editor
+{
+    [MenuItem("SceneSelector/Main _F1")]
+    static public void OpenMainScene()
+    {
+        OpenScene("Assets/GameAssets/Scenes/Main.unity");
+    }
+
+    [MenuItem("SceneSelector/AnimationEditor _F2")]
+    static public void OpenAnimationEditorScene()
+    {
+        OpenScene("Assets/GameAssets/Scenes/AnimationEditor.unity");
+    }
+
+    private static void OpenScene(string scene_path)
+    {
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene(scene_path);
+        }
+    }
+
+}
