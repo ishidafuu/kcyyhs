c3c5ea72e4a4cee4f2cf02d2529e436ba184e4fa
ishidafuu <ishidafuu@gmail.com>
2019-09-08 14:34:54 +0900

    [remove] EasyTouch不要なので削除

--- Assets/GameAssets/Scripts/ECS/Systems/00Scan/PadScanSystem.cs
+++ Assets/GameAssets/Scripts/ECS/Systems/00Scan/PadScanSystem.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using HedgehogTeam.EasyTouch;
 using Unity.Burst;
 using Unity.Collections;
 using Unity.Entities;
